7896f844472f86fdc9327e8be744fe741753fecb
Pavel <white03041996@gmail.com>
2023-07-21 14:21:27 +0300

    fix task3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,18 @@
-"use strict"
+"use strict"; 
+   
 function solveEquation(a, b, c) {
   let arr = [];
-  const discriminant = Math.pow(b, 2) - 4 * a * c;
-  
-  if (discriminant > 0) {
-    let arr1 = (-b + Math.sqrt(discriminant) ) / (2 * a);
-    let arr2 = (-b - Math.sqrt(discriminant) ) / (2 * a);
-    arr.push (arr1, arr2);
-  } else if (discriminant === 0) {
-    let arr3 = -b / (2 * a);
-    arr.push(arr);
+  let d = (b**2-4*a*c);
+  if (d>0){
+   let root1 = (-b + Math.sqrt(d) )/(2*a);
+   let root2 =  (-b - Math.sqrt(d) )/(2*a);
+   arr.push(root1,root2);
+   return arr;
+  }
+
+if (d === 0){
+  let root0= -b/(2*a);
+  arr.push(root0);
   }
   return arr;
 }
