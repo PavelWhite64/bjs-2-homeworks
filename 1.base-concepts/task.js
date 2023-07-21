f78709f4032ae496d98a3dc0fa1ed7bba8d775fb
Pavel <white03041996@gmail.com>
2023-07-21 14:18:51 +0300

    fix task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,25 +14,7 @@ function solveEquation(a, b, c) {
   return arr;
 }
 
-// function setNumber() {
-//   const a = parseFloat(prompt("Введите коэффициент a: "));
-//   const b = parseFloat(prompt("Введите коэффициент b: "));
-//   const c = parseFloat(prompt("Введите коэффициент c: "));
 
-//   if (isNaN(a) || isNaN(b) || isNaN(c)) {
-//     console.log("Ошибка: введите корректное число");
-//   }
-
-  let arr = solutionResult(a, b, c);
-
-  if (arr.length === 0) {
-    console.log("Нет корней");
-  } else if (arr.length === 1) {
-    console.log(`Имеет один корень: х = ${arr[0]}`);
-  } else if (arr.length === 2) {
-    console.log(`Имеет два корня: х1 = ${arr[0]}, x2 = ${arr[1]}`);
-  }
-}
 // function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 // }
