5d43af3c8b8001a9fbdc839cdaa2726f2ccb189f
Pavel <white03041996@gmail.com>
2023-07-29 11:36:04 +0300

    Finish

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+	return ((arr1.length === arr2.length) && arr1.every((elem, i) => elem === arr2[i]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(old => old.age).reduce((acc, old, i, arr) => {
+		acc += old;
+		if (i === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+	}, 0);
+	return result;
 }
