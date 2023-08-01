70910454783a2f762e16ed988d1e39db02e2d516
Pavel <white03041996@gmail.com>
2023-08-01 10:45:56 +0300

    Finish

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,24 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  (this.marks === undefined) ? this.marks = undefined : this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  return (this.marks === undefined) ? this.marks = 0 : this.marks.reduce((acc, marks, index, arr) => acc + marks / arr.length, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
